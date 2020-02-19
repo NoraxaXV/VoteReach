bdbdf5a6c22fd1bdd51ff316013ec60148505ba6
RebornMaster <asmith10@dekalbcentral.net>
2020-02-19 20:15:47 +0000

    fixed cookie.js

--- cookie.js
+++ cookie.js
@@ -10,7 +10,7 @@ var Cookie = {
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+  },
 
   // Gets a cookie by its name. Returns either a value or an empty string
   get: function (cname) {
