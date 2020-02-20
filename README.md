fb183fd5a2c05713d68374d36d2b6ca7efe9911c
RebornMaster <asmith10@dekalbcentral.net>
2020-02-20 21:25:58 +0000

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #VoteReach
+The main file for the server is _main.py_
+
+_JS_ and _CSS_ files go in the _'static'_ folder
+_Html_ files go in the _'templates'_ folder
+
+
 [![Run on Repl.it](https://repl.it/badge/github/RebornMaster/VoteReach)](https://repl.it/github/RebornMaster/VoteReach)
--- main.py
+++ main.py
@@ -1,34 +1,46 @@
+#   This is the code for the server
+
+#Imports modules
 from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask('app')
+#Creates the server object
+app = Flask("app")
 
 
-@app.route("/linker")
-def linkPage():
-  
-  return render_template("linkerIndex.html")
-
-
+#This returns default page
 @app.route("/")
 def main():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
-@app.route('/signup', methods=['POST', 'GET'])
+#This returns the link page
+@app.route("/linker")
+def linkPage():
+    return render_template("linkerIndex.html")
+
+
+#This returns the signup page and gets form requests
+@app.route("/signup", methods=["GET"])
 def signup():
-  return render_template('sign_up.html')
+    return render_template("sign_up.html")
 
 
+#This returns the leaderboard
 @app.route("/leaderboard")
 def leaderboard():
-  return render_template("leaderboard.html")
+    return render_template("leaderboard.html")
 
-@app.route("/signup/confirm", methods=['POST', 'GET'])
+
+#This is the page that will send the email
+@app.route("/signup/confirm", methods=["POST", "GET"])
 def sendEmail():
-  if request.method == "POST":
-    return render_template("check_email.html", username=request.form["username"])
+    if request.method == "POST":
+        return render_template(
+            "check_email.html", username=request.form["username"])
 
+
+#Finally, start up the server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
