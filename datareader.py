2c40740272633be4abdddcfc83a38472ed212b31
RebornMaster <asmith10@dekalbcentral.net>
2020-02-25 13:06:00 +0000

    added leaderboard, fixed caching problem

--- datareader.py
+++ datareader.py
@@ -1,14 +1,14 @@
 # This contains the files for reading and writing user data so we can keep track of clicks
 #Data is stored like this:
-#name1 clicks
-#name2 clicks
+#name1|clicks
+#name2|clicks
 #..
 import os
 
 # Adds a user to the file
 def addUser(username):
-  with open("userdata.txt","w") as file:
-    file.write("%s 0" % username)
+  with open("userdata.txt","a") as file:
+    file.write("%s|0 \n" % username)
 
 # Returns the number of clicks a user has, given his username. Returns -1 if user was not found
 def getUserClicks(username):  
@@ -16,7 +16,7 @@ def getUserClicks(username):
     data = f.readlines()
 
     for line in data:
-      words=line.split()
+      words=line.split("|")
       if words[0]==username:
         return words[1]
   return -1      
@@ -25,7 +25,7 @@ def getUserClicks(username):
 # Returns the data at a line number
 def getAtLine(i):
   with open("userdata.txt", "r") as f:
-    return f.readLines()[i].split()
+    return f.readLines()[i].strip().split("|")
 
 # Increments the clicks of a user
 def incrementUserClicks(username):    
@@ -33,17 +33,28 @@ def incrementUserClicks(username):
   with open("userdata.txt", "r") as fin, open("userdata.txt.tmp", "w+") as fout:
     for line in fin:
       lineout = line
-      words=line.split()
+      words=line.split("|")
       
       name=words[0]
       clicks=words[1]
       
       if name == username:
-        lineout = "%s %s\n" %(name, int(clicks)+1)
+        lineout = "%s|%s\n" %(name, int(clicks)+1)
       
       fout.write(lineout)
 
   os.rename("userdata.txt.tmp", "userdata.txt")
 
+def getAllData():
+  dataList=[] 
+  
+  with open("userdata.txt", "r") as data:
+    for fullLine in data:
+      splitLine = fullLine.split("|")
+      name=splitLine[0]
+      numOfClicks=int(splitLine[1])
+      dataList.append({"username": name, "clicks": numOfClicks})
+  
+  return dataList
 
 
--- main.py
+++ main.py
@@ -15,9 +15,10 @@ from datareader import *
 # Creates the server object (Doesn't start it yet, though)
 app = Flask("app")
 app.secret_key=os.getenv("APP_SECRET_KEY")
+
 # Gets config file
 app.config.from_pyfile('config.py')
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 #This returns default page
 @app.route("/")
@@ -30,6 +31,10 @@ def main():
 def linkPage():
   return render_template("linkerIndex.html")
 
+#This returns the link page
+@app.route("/test_bed")
+def test_bed():
+  return render_template("test_bed.html")
 
 #This returns the signup page and gets form requests
 @app.route("/signup", methods=["GET"])
@@ -40,7 +45,9 @@ def signup():
 #This returns the leaderboard
 @app.route("/leaderboard")
 def leaderboard():
-  return render_template("leaderboard.html")
+  sortedClickData=sorted(getAllData(), key=lambda i: (i["clicks"], i["username"]), reverse=True)
+  
+  return render_template("leaderboard.html", data=sortedClickData)
 
 
 #This is the page that will send the email
@@ -49,20 +56,16 @@ def sendEmail():
   if request.method == "POST":
     username=request.form["username"]
     if getUserClicks(username) != -1:
-      flash("Username taken.")
       return redirect(url_for("signup"))
-    else:
-      addUser(username)
+
+    addUser(username)
     
     #This contains the email to be sent
     message = Mail(
       from_email="asmith10@dekalbcentral.net",
       to_emails=request.form["email"],
       subject="Here is the link to the website!",
-      html_content='''
-      <p>Thanks for doing this, %s! Now go, copy/paste the link below, share this, and get as many clicks as you can! </p>
-      <a href="https://votereach.aaronsmith8.repl.co/register-to-vote/%s">https://votereach.aaronsmith8.repl.co/register-to-vote/%s</a>
-      ''' % (username,username,username))
+      html_content=render_template("email.html", username=username))
       
       #This sends the email
     try:
@@ -83,10 +86,20 @@ def updateLink(id):
     return "Link no longer valid!"
   else:
     incrementUserClicks(id)   
-  
+    
   return redirect("https://www.usa.gov/register-to-vote")
 
-
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers["Cache-Control;"] = "public, max-age=0"
+    return r
 
 #______________________________________Finally, start up the server
 if __name__ == "__main__":
